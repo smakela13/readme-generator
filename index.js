47574a7caccc0179961027fb6dea1b606c0ac596
SMakela <73409799+smakela13@users.noreply.github.com>
2021-04-05 15:41:23 -0400

    Created questions for the user to answer, wrote function for README file and wrote the init function, also added the required packages

--- index.js
+++ index.js
@@ -1,13 +1,67 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const utils = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Describe your project.',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Write instructions to describe how to install your project.',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Describe how to use your project.',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Provide examples on how to test your project.',
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Input guidelines on how one would contribute to your project.',
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Provide examples on how to test your project.',
+    },
+    {
+        type: 'input',
+        name: 'questions',
+        message: 'Let the user know how they can ask questions.',
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, answers) {
+    fs.writeFile(fileName, answers);
+ }
+
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    console.log('To get started, please answer the following questions about your application:');
+    inquirer.prompt(questions)
+        .then((answers) => {
+            const userContent = utils(answers);
+        writeToFile('README.md', userContent);
+    });
+}
 
 // Function call to initialize app
 init();
